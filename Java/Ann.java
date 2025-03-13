e656fa1e8ffb5a541ef78b8d5e6d037ce3214873
ann martinez <105459417+amarti360@users.noreply.github.com>
2025-03-13 19:53:18 -0400

    printing initials in big letters with *

---
+++ Java/Ann.java
@@ -0,0 +1,18 @@
+package projectAnn;
+
+public class Ann {
+	/* This program print my initials (AM) in big letters, where each letter is nine lines tall.
+	*/
+	
+	public static void main(String[] args) {
+		System.out.println("        *                *              *");
+		System.out.println("       * *               * *          * *");
+		System.out.println("      *   *              *  *        *  *");
+		System.out.println("     *     *             *   *     *    *");
+		System.out.println("    *********            *      *       *");
+		System.out.println("   *         *           *              *");
+		System.out.println("  *           *          *              *");
+		System.out.println(" *             *         *              *");
+		System.out.println("*               *        *              *");
+		}
+} // end class Ann
